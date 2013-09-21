083ef290767ba4527419452cc1227dae0a8ac19e
marcbaechinger <marc.baechinger@gmail.com>
2013-09-21 17:42:32 +0200

    [step-1]: implemented requirements of first step

--- public_html/js/controller.js
+++ public_html/js/controller.js
@@ -1,4 +1,16 @@
 /*global alert: false */
 /*jslint browser: true */
 (function (global) {
+	var Controller = function (options) {
+		var container = document.getElementById(options.id);
+		
+		container.addEventListener("click", function (ev) {
+			var action = ev.target.dataset.action;
+			if (action) {
+				alert(action);
+			}
+		}, false);
+	};
+	
+	global.Controller = Controller;
 }(this));
--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,8 @@
-/*global controllers: false, alert: false */
+/*global Controller: false, alert: false */
 /*jshint browser: true */
 (function (global) {
+	
+	var controller = new Controller({
+		id: "players"
+	});
 }(this));
