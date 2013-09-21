7fe8d0e869305505b2184de37c29d748cc40aaf1
marcbaechinger <marc.baechinger@gmail.com>
2013-09-21 18:04:36 +0200

    added detail page. Let detail page slide in on click on the badges in the list view.  Slide detail page out on click on large badge.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,11 +2,22 @@
 /*jshint browser: true */
 (function (global) {
 	
+	var body = document.querySelector("body");
+	
 	var controller = new Controller({
 		id: "players",
 		actions: {
 			show: function(ev) {
-				alert("delegated");
+				body.classList.add("page-detail");
+			}
+		}
+	});
+	
+	var detailController = new Controller({
+		id: "detail",
+		actions: {
+			hide: function () {
+				body.classList.remove("page-detail");
 			}
 		}
 	});
